cee7cb329d904bdfe76029369eaecce5fe8d2bd1
oleksiinarinskyi <oleksii.narinskyi@gmail.com>
2023-02-02 12:58:46 +1100

    Add testCase2

--- src/test/java/com/planittesting/cloud/jupiter/TestSuite.java
+++ src/test/java/com/planittesting/cloud/jupiter/TestSuite.java
@@ -5,6 +5,7 @@ import com.planittesting.cloud.jupiter.data.component.Alert;
 import com.planittesting.cloud.jupiter.data.component.Input;
 import com.planittesting.cloud.jupiter.data.component.NavigationBar;
 import com.planittesting.cloud.jupiter.data.page.ContactPage;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -30,13 +31,47 @@ public class TestSuite extends AbstractUITest {
         List<String> inputErrors = driver.getElementsText(Input.ERROR_MESSAGE);
         assertThat(inputErrors, hasItems("Forename is required", "Email is required", "Message is required"));
 
-        driver.sendKeys(Input.FORENAME, "ØJohn");
+        driver.sendKeys(Input.FORENAME, "John");
         driver.sendKeys(Input.EMAIL, "john@doe.com");
-        driver.sendKeys(Input.MESSAGE, "!");
+        driver.sendKeys(Input.MESSAGE, "Message");
 
         String alertInfo = driver.getElementText(Alert.Info.BODY);
         softly.assertTrue(alertInfo.contains("We welcome your feedback"));
         softly.assertTrue(alertInfo.contains("- tell it how it is."));
         softly.assertAll();
     }
+
+    @DataProvider(name = "users")
+    public Object[][] users() {
+        return new Object[][] {
+                { "a", "a@b.cd", "a"},
+                { "Z z", "Z.z@z.com", "Z"},
+                { "A z z", "A.z.z@bc.def", "A z z "},
+                { "1!@#$%^&~**()<>|/]{0", "test.email@gmail.com", "1!@#$%^&~**()<>|/]{0"},
+                { "My name is long it's just long it's what they call me it's a long name",
+                        "myemail.is.unreasonably.long@at.a.very.long.domain.com",
+                        "This is a very long message which should be decided with stakeholders based on \n" +
+                                "what the actual requirements are"},
+        };
+    }
+
+    @Test(dataProvider = "users")
+    public void testCase2(String forename, String email, String message) {
+        openHomePage();
+
+        driver.clickOn(NavigationBar.CONTACT);
+
+        driver.sendKeys(Input.FORENAME, forename);
+        driver.sendKeys(Input.EMAIL, email);
+        driver.sendKeys(Input.MESSAGE, message);
+
+        driver.clickOn(ContactPage.BUTTON_SUBMIT);
+
+        String alertInfo = driver.getElementText(Alert.Info.BODY);
+        softly.assertTrue(alertInfo.contains("We welcome your feedback"));
+        softly.assertTrue(alertInfo.contains("- tell it how it is."));
+        softly.assertAll();
+    }
+
+
 }
